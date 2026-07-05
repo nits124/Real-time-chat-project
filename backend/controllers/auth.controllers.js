2c0cc9916361e3b4998476b626d272fbca39a6a4
nitin <165454539+nits124@users.noreply.github.com>
2026-07-05 18:30:43 +0530

    'sss'

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -56,8 +56,8 @@ export const login=async (req,res)=>{
  res.cookie("token",token,{
      httpOnly:true,
      maxAge:7*24*60*60*1000,
-     sameSite:"Strict",
-     secure:false
+     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+     secure: process.env.NODE_ENV === "production"
     })
  
     return res.status(200).json(user)
